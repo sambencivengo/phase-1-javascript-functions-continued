ed1e1f949cca06c3dec65462fa38e797bb03c8d8
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-21 08:27:06 -0400

    commit tree b8decbd9db35727e7baab3c5553db12f8b0a0dc7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(val = 'roller-skate') {
+    return `This Saturday, I want to ${val}!`
+}
+
+function mondayWork(val = 'go to the office'){
+    return `This Monday, I will ${val}.`;
+}
+
+function wrapAdjective(flair = "*", String){
+    return function innerFunction(val = "special"){
+        return `You are ${flair}${val}${flair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!")
